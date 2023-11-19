4a332b25c1fa057b308842e7772599eae2e984f3
oMFDOo <50069569+oMFDOo@users.noreply.github.com>
2023-11-20 03:37:11 +0900

    add: Board Insert Dto

--- EnjoyTripFinal/src/main/java/com/project/enjoytrip/board/controller/BoardController.java
+++ EnjoyTripFinal/src/main/java/com/project/enjoytrip/board/controller/BoardController.java
@@ -1,5 +1,33 @@
 package com.project.enjoytrip.board.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.enjoytrip.board.dto.BoardInsertDto;
+import com.project.enjoytrip.board.service.BoardService;
+
+@RestController
 public class BoardController {
+	
+
+	private BoardService boardService;
+
+	public BoardController(BoardService boardService) {
+		this.boardService = boardService;
+	}
+
+	@PostMapping(value = "/board")
+	public Map<String, String> Follow(BoardInsertDto boardInsertDto, int userId) {
+		Map<String, String> map = new HashMap<>();
+		if (boardService.Insert(boardInsertDto, userId)) {
+			map.put("Board", "SUCCESS");
+		} else {
+			map.put("Board", "FAIL");
+		}
+		return map;
+	}
 
 }
